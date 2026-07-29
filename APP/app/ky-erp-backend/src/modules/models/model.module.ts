bbc89c0da0d4cad57e16da2dfd913535958235bd
cetin kaya <cetin60kaya@gmail.com>
2026-07-29 08:28:41 +0300

    İşNet üretim plan senkron servisini modüle bağla

--- APP/app/ky-erp-backend/src/modules/models/model.module.ts
+++ APP/app/ky-erp-backend/src/modules/models/model.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../../database/database.module";
+import { CanonicalDispatchSyncService } from "./canonical-dispatch-sync.service";
 import { CanonicalModelFlowController } from "./canonical-model-flow.controller";
 import { CanonicalModelFlowService } from "./canonical-model-flow.service";
 import { ModelService } from "./model.service";
@@ -7,7 +8,7 @@ import { ModelService } from "./model.service";
 @Module({
   imports: [DatabaseModule],
   controllers: [CanonicalModelFlowController],
-  providers: [ModelService, CanonicalModelFlowService],
-  exports: [ModelService, CanonicalModelFlowService],
+  providers: [ModelService, CanonicalModelFlowService, CanonicalDispatchSyncService],
+  exports: [ModelService, CanonicalModelFlowService, CanonicalDispatchSyncService],
 })
 export class ModelModule {}
