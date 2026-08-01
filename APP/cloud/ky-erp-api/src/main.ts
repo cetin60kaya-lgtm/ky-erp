2bb4e37e4b9fc4dc0468152ca2fa4b34bd406582
cetin kaya <cetin60kaya@gmail.com>
2026-08-01 20:22:12 +0300

    feat(boyahane): iş, reçete ve üretim rotalarını Worker'a bağla

--- APP/cloud/ky-erp-api/src/main.ts
+++ APP/cloud/ky-erp-api/src/main.ts
@@ -1,8 +1,10 @@
 import app from "./index";
 import { registerBoyahaneInventoryRoutes } from "./boyahane-inventory";
+import { registerBoyahaneWorkflowRoutes } from "./boyahane-workflow";
 import { registerProductionCenterRoutes } from "./production-center";
 
 registerProductionCenterRoutes(app);
 registerBoyahaneInventoryRoutes(app);
+registerBoyahaneWorkflowRoutes(app);
 
 export default app;
