1b7fe874518b7c50338bb935cae34ef0c3f9f8df
cetin kaya <cetin60kaya@gmail.com>
2026-08-01 20:25:09 +0300

    chore(worker): iş modüllerinin kayıt sırasını belgeleyip testi yenile

--- APP/cloud/ky-erp-api/src/main.ts
+++ APP/cloud/ky-erp-api/src/main.ts
@@ -3,6 +3,7 @@ import { registerBoyahaneInventoryRoutes } from "./boyahane-inventory";
 import { registerBoyahaneWorkflowRoutes } from "./boyahane-workflow";
 import { registerProductionCenterRoutes } from "./production-center";
 
+// Önce ortak üretim ve stok kaynakları, ardından bu kaynakları kullanan iş akışları bağlanır.
 registerProductionCenterRoutes(app);
 registerBoyahaneInventoryRoutes(app);
 registerBoyahaneWorkflowRoutes(app);
