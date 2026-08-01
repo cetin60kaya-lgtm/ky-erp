e0408e80666ea87f2355eebb005dfd2788e4f527
cetin kaya <cetin60kaya@gmail.com>
2026-08-01 20:01:34 +0300

    feat(boyahane): onaylı ürün ve lot stok rotalarını Worker'a bağla

--- APP/cloud/ky-erp-api/src/main.ts
+++ APP/cloud/ky-erp-api/src/main.ts
@@ -1,6 +1,8 @@
 import app from "./index";
+import { registerBoyahaneInventoryRoutes } from "./boyahane-inventory";
 import { registerProductionCenterRoutes } from "./production-center";
 
 registerProductionCenterRoutes(app);
+registerBoyahaneInventoryRoutes(app);
 
 export default app;
