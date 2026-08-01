559b511e17be80cdaab4a34d45581478dba63327
cetin kaya <cetin60kaya@gmail.com>
2026-08-01 20:33:40 +0300

    feat(desen): Desen workflow ve Boyahane senkron rotalarını Worker'a bağla

--- APP/cloud/ky-erp-api/src/main.ts
+++ APP/cloud/ky-erp-api/src/main.ts
@@ -1,11 +1,13 @@
 import app from "./index";
 import { registerBoyahaneInventoryRoutes } from "./boyahane-inventory";
 import { registerBoyahaneWorkflowRoutes } from "./boyahane-workflow";
+import { registerDesenWorkflowRoutes } from "./desen-workflow";
 import { registerProductionCenterRoutes } from "./production-center";
 
 // Önce ortak üretim ve stok kaynakları, ardından bu kaynakları kullanan iş akışları bağlanır.
 registerProductionCenterRoutes(app);
 registerBoyahaneInventoryRoutes(app);
 registerBoyahaneWorkflowRoutes(app);
+registerDesenWorkflowRoutes(app);
 
 export default app;
