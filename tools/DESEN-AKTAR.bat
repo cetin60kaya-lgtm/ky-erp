53879b99012aaf13f5f9bc2e06309d47d4b89654
cetin kaya <cetin60kaya@gmail.com>
2026-08-09 12:24:09 +0300

    chore: remove obsolete bulk design migration launcher

--- tools/DESEN-AKTAR.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-title KY ERP - Desen Bulut Aktarimi
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0DESEN-AKTAR.ps1"
-set "EXITCODE=%ERRORLEVEL%"
-endlocal & exit /b %EXITCODE%
