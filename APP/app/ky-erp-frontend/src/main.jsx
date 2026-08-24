534157cb1c211ae16e3cfcb9f6154c52f564ab9e
cetin kaya <cetin60kaya@gmail.com>
2026-08-24 08:23:38 +0300

    fix(frontend): install authenticated asset bridge

--- APP/app/ky-erp-frontend/src/main.jsx
+++ APP/app/ky-erp-frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import AppV3 from "./AppV3.jsx";
 import { ActiveCompanyProvider } from "./context/ActiveCompanyContext";
 import { AuthProvider } from "./context/AuthContext";
+import { installAuthenticatedAssetBridge } from "./utils/installAuthenticatedAssetBridge";
 import { installMuhasebeDocumentSanitizer } from "./utils/installMuhasebeDocumentSanitizer";
 import { installPersistentModalSizing } from "./utils/installPersistentModalSizing";
 import "./App.css";
 import "./styles/shell-v3-isnet.css";
 
+installAuthenticatedAssetBridge();
 installMuhasebeDocumentSanitizer();
 installPersistentModalSizing();
 
