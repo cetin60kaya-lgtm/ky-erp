f147e488f11f2d20b772d509d5d757bc40af5d1b
cetin kaya <cetin60kaya@gmail.com>
2026-09-02 18:28:07 +0300

    refactor(storage): make backend worker repo-relative

--- APP/app/KYERP_BACKEND_WORKER.cmd
+++ APP/app/KYERP_BACKEND_WORKER.cmd
@@ -1,5 +1,5 @@
 @echo off
 title KY ERP BACKEND
-cd /d "D:\onedrive-Hkn\OneDrive\KY-ERP-MERKEZ\APP\app\ky-erp-backend"
+cd /d "%~dp0ky-erp-backend"
 echo [%date% %time%] Backend basladi >> "D:\KYERP-YEDEK\LOGS\KYERP_BACKEND.log"
 call npm.cmd run start:dev >> "D:\KYERP-YEDEK\LOGS\KYERP_BACKEND.log" 2>&1
