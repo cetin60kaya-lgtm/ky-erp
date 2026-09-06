837a6c0d1559557ec17770dd171f26d3fd3b9300
cetin kaya <cetin60kaya@gmail.com>
2026-09-06 13:49:43 +0300

    fix(users): remove embedded login approvals from user center

--- APP/app/ky-erp-frontend/src/pages/admin/AdminUsersPanel.jsx
+++ APP/app/ky-erp-frontend/src/pages/admin/AdminUsersPanel.jsx
@@ -1,12 +1,5 @@
-import AdminLoginApprovals from "./AdminLoginApprovals";
 import AdminUsersPanelV2 from "./AdminUsersPanelV2";
-import "./AdminUsersPanelHardening.css";
 
 export default function AdminUsersPanel(props) {
-  return (
-    <div style={{ display: "grid", gap: 16 }}>
-      <AdminLoginApprovals compact />
-      <AdminUsersPanelV2 {...props} />
-    </div>
-  );
+  return <AdminUsersPanelV2 {...props} />;
 }
