99a3f3bfa44884f2774d54a52d65e2d1b8013f1b
cetin kaya <cetin60kaya@gmail.com>
2026-09-07 11:18:29 +0300

    chore(pdks): 1.8 dual ürün kaynağını güncel tabana taşı

--- APP/desktop/ky-pdks/src/KyPdks.Desktop/App.xaml.cs
+++ APP/desktop/ky-pdks/src/KyPdks.Desktop/App.xaml.cs
@@ -4,4 +4,15 @@ namespace KyPdks.Desktop;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+#if PDKS_ONLY
+        var window = new PdksWorkbenchWindow();
+#else
+        var window = new KyErpDesktopWindow();
+#endif
+        MainWindow = window;
+        window.Show();
+    }
 }
